d3614e24929df9c96b92c4e657db23cb1dea7659
Asela Bandara Samarakoon <samarakoon2@gmail.com>
2018-10-08 13:29:50 +0530

    Add project files.

---
+++ Asela.Samples.VueJs.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Asela.Samples.VueJs.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
